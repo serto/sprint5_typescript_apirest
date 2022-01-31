d0ff42d771535d3b817b40db2d0e208d64508d4b
Sertorio Morales <sertomorales@gmail.com>
2022-01-31 16:31:47 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const world = 'world';
+const API_URL = "";
+function hello(world) {
+    console.log('hola testing');
+    return `Hello ${world}! `;
+}
+exports.hello = hello;
+hello('hola ');
+//# sourceMappingURL=index.js.map
---
+++ src/typescript/index.ts
@@ -0,0 +1,11 @@
+const world = 'world';
+
+const API_URL = "";
+
+
+export function hello(world: string): string {
+  console.log('hola testing');
+  return `Hello ${world}! `;
+}
+
+hello('hola ');
