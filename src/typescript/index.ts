4fe3bcd2aeeee28c9376d9a07fd3d65fd9d41029
Sertorio Morales <sertomorales@gmail.com>
2022-02-01 18:09:25 +0100

    sol exercici 3

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,14 +1,14 @@
 
 const API_URL = "https://icanhazdadjoke.com/";
 
-
-var headersFetch = { 
+const headersFetch = { 
                     headers:{
                       'Accept': 'application/json',
                       'Content-Type': 'application/json'
                     }
                    };
 
+const reportJokes:Object[] = [];
 
 export function getAJoke(): void {
 
@@ -25,9 +25,44 @@ export function getAJoke(): void {
                 const p = document.createElement("p");
                 p.textContent = joke.joke;
                 p.setAttribute('id', 'jokeFetch');
+                p.setAttribute('idJoke',  joke.id);
                 divJoke?.appendChild(p);
+
+                document.getElementById('jokeRate')?.classList.add("show");
 
               });
 }
 
+export async function puntJoke(elem: EventTarget): void {
+  
+  const punt = elem.dataset.punt;
+
+  const date = new Date();
+  const dateIso = date.toISOString();
+
+  const idJoke = document.getElementById("jokeFetch")?.getAttribute('idJoke');
+  const textJoke = document.getElementById("jokeFetch")?.innerHTML;
+
+  const jokeToAdd = {
+    id: idJoke,
+    joke: textJoke
+  }
+
+  const newElement = {
+    joke: jokeToAdd,
+    score: punt,
+    date: dateIso
+  };
+
+  reportJokes.push(newElement);
+
+  console.log('reportJokes : ', reportJokes);
+}
+
+
+
 document.getElementById('getJoke').addEventListener('click', getAJoke);
+
+document.getElementById('point-neg').addEventListener('click', (e) => { puntJoke(e.target); });
+document.getElementById('point-zero').addEventListener('click', (e) => { puntJoke(e.target); });
+document.getElementById('point-pos').addEventListener('click', (e) => { puntJoke(e.target); });
