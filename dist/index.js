04bb33dbaaf54503be538f68a66f6e0601581a6a
Sertorio Morales <sertomorales@gmail.com>
2022-02-02 17:01:50 +0100

    sol ex 4, 5 i 6

--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,26 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var API_URL = "https://icanhazdadjoke.com/";
+var API_WEATHER = "http://api.weatherstack.com/current";
+var API_WEATHER_KEY = "b98db4ac3beb81feecf2ed523b254135";
+var API_CHUCKJOKES = "https://api.chucknorris.io/jokes/random";
 var headersFetch = {
     headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+    }
+};
+var headersFetchCors = {
+    mode: 'cors',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
     }
 };
 var reportJokes = [];
 export function getAJoke() {
-    var joke = fetch("".concat(API_URL), headersFetch)
+    var randomAPI = Math.floor(Math.random() * 2);
+    var API_To_Call = (randomAPI == 0) ? API_URL : API_CHUCKJOKES;
+    fetch("".concat(API_To_Call), headersFetch)
         .then(function (response) { return response.json(); })
         .then(function (joke) {
         var _a;
@@ -52,38 +29,51 @@ export function getAJoke() {
             divJoke.innerHTML = " ";
         }
         var p = document.createElement("p");
-        p.textContent = joke.joke;
+        var joke_text = (randomAPI == 0) ? joke.joke : joke.value;
+        p.textContent = joke_text;
         p.setAttribute('id', 'jokeFetch');
         p.setAttribute('idJoke', joke.id);
         divJoke === null || divJoke === void 0 ? void 0 : divJoke.appendChild(p);
         (_a = document.getElementById('jokeRate')) === null || _a === void 0 ? void 0 : _a.classList.add("show");
+        randomBack();
     });
+}
+function randomBack() {
+    var randomAPI = Math.floor(Math.random() * 4);
+    var randomBack = "jokes__back--back".concat(randomAPI);
+    var divBackground = document.getElementById('jokesBackground');
+    divBackground === null || divBackground === void 0 ? void 0 : divBackground.classList.remove("jokes__back--back1");
+    divBackground === null || divBackground === void 0 ? void 0 : divBackground.classList.remove("jokes__back--back2");
+    divBackground === null || divBackground === void 0 ? void 0 : divBackground.classList.remove("jokes__back--back3");
+    divBackground === null || divBackground === void 0 ? void 0 : divBackground.classList.add(randomBack);
 }
 export function puntJoke(elem) {
     var _a, _b;
-    return __awaiter(this, void 0, void 0, function () {
-        var punt, date, dateIso, idJoke, textJoke, jokeToAdd, newElement;
-        return __generator(this, function (_c) {
-            punt = elem.dataset.punt;
-            date = new Date();
-            dateIso = date.toISOString();
-            idJoke = (_a = document.getElementById("jokeFetch")) === null || _a === void 0 ? void 0 : _a.getAttribute('idJoke');
-            textJoke = (_b = document.getElementById("jokeFetch")) === null || _b === void 0 ? void 0 : _b.innerHTML;
-            jokeToAdd = {
-                id: idJoke,
-                joke: textJoke
-            };
-            newElement = {
-                joke: jokeToAdd,
-                score: punt,
-                date: dateIso
-            };
-            reportJokes.push(newElement);
-            console.log('reportJokes : ', reportJokes);
-            return [2 /*return*/];
-        });
+    var punt = elem.dataset.punt;
+    var date = new Date();
+    var dateIso = date.toISOString();
+    var idJoke = (_a = document.getElementById("jokeFetch")) === null || _a === void 0 ? void 0 : _a.getAttribute('idJoke');
+    var textJoke = (_b = document.getElementById("jokeFetch")) === null || _b === void 0 ? void 0 : _b.innerHTML;
+    var jokeToAdd = {
+        id: idJoke,
+        joke: textJoke
+    };
+    var newElement = {
+        joke: jokeToAdd,
+        score: punt,
+        date: dateIso
+    };
+    reportJokes.push(newElement);
+    console.log('reportJokes : ', reportJokes);
+}
+function weather() {
+    fetch("".concat(API_WEATHER, "?access_key=").concat(API_WEATHER_KEY, "&query=Barcelona"), headersFetchCors)
+        .then(function (response) { return response.json(); })
+        .then(function (wheater) {
+        console.log('works ? : ', wheater);
     });
 }
+weather();
 document.getElementById('getJoke').addEventListener('click', getAJoke);
 document.getElementById('point-neg').addEventListener('click', function (e) { puntJoke(e.target); });
 document.getElementById('point-zero').addEventListener('click', function (e) { puntJoke(e.target); });
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,18 +1,35 @@
 
 const API_URL = "https://icanhazdadjoke.com/";
+const API_WEATHER = "http://api.weatherstack.com/current";
+const API_WEATHER_KEY = "b98db4ac3beb81feecf2ed523b254135";
+
+const API_CHUCKJOKES = "https://api.chucknorris.io/jokes/random";
 
 const headersFetch = { 
                     headers:{
                       'Accept': 'application/json',
-                      'Content-Type': 'application/json'
+                      'Content-Type': 'application/json',
                     }
                    };
+
+const headersFetchCors = { 
+                    mode: 'cors',
+                    headers:{
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin': '*'
+                    }
+                  };
 
 const reportJokes:Object[] = [];
 
 export function getAJoke(): void {
 
-  const joke = fetch(`${API_URL}`, headersFetch)
+  const randomAPI = Math.floor(Math.random() * 2);
+
+  const API_To_Call = (randomAPI == 0) ? API_URL : API_CHUCKJOKES;
+
+  fetch(`${API_To_Call}`, headersFetch)
               .then(response => response.json())
               .then( (joke) => {
 
@@ -23,17 +40,37 @@ export function getAJoke(): void {
                 }
 
                 const p = document.createElement("p");
-                p.textContent = joke.joke;
+
+                const joke_text = (randomAPI == 0) ? joke.joke : joke.value;
+
+                p.textContent = joke_text;
                 p.setAttribute('id', 'jokeFetch');
                 p.setAttribute('idJoke',  joke.id);
                 divJoke?.appendChild(p);
 
                 document.getElementById('jokeRate')?.classList.add("show");
 
+                randomBack();
+
               });
 }
 
-export async function puntJoke(elem: EventTarget): void {
+function randomBack() {
+
+  let randomAPI = Math.floor(Math.random() * 4);
+  const randomBack = `jokes__back--back${randomAPI}`;
+
+  const divBackground = document.getElementById('jokesBackground');
+
+  divBackground?.classList.remove("jokes__back--back1");
+  divBackground?.classList.remove("jokes__back--back2");
+  divBackground?.classList.remove("jokes__back--back3");
+  divBackground?.classList.add(randomBack);
+
+
+}
+
+export function puntJoke(elem: EventTarget): void {
   
   const punt = elem.dataset.punt;
 
@@ -59,7 +96,17 @@ export async function puntJoke(elem: EventTarget): void {
   console.log('reportJokes : ', reportJokes);
 }
 
+function weather(): void {
+  fetch(`${API_WEATHER}?access_key=${API_WEATHER_KEY}&query=Barcelona`, headersFetchCors)
+              .then(response => response.json())
+              .then( (wheater) => {
 
+                console.log('works ? : ', wheater);
+
+              }); 
+}
+
+weather();
 
 document.getElementById('getJoke').addEventListener('click', getAJoke);
 
